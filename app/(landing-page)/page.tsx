d86f9ab98c006fa6040cc396ea164a366afec9f9
Greg Barajas <gregbarajas96@gmail.com>
2023-11-10 09:04:10 -0800

    Added fourth section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,7 +1,9 @@
 import FirstSection from "./FirstSection/page";
+import FourthSection from "./FourthSection/page";
 import Logo from "./Navbar/_components/logo";
 import Navbar from "./Navbar/navbar";
 import SecondSection from "./SecondSection/page";
+import ThirdSection from "./ThirdSection/page";
 
 
 const LandingPage = () => {
@@ -10,6 +12,8 @@ const LandingPage = () => {
       <Navbar/>
       <FirstSection/>
       <SecondSection/>
+      <ThirdSection/>
+      <FourthSection/>
     </div>
   );
 }
