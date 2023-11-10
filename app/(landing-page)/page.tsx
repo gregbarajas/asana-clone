4907214fe3bb494c7c3729c5126089bf37798446
Greg Barajas <gregbarajas96@gmail.com>
2023-11-10 09:54:46 -0800

    Added Sixth, Seventh, Eighth Section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,9 +1,12 @@
+import EighthSection from "./EighthSection/page";
 import FifthSection from "./FifthSection/page";
 import FirstSection from "./FirstSection/page";
 import FourthSection from "./FourthSection/page";
 import Logo from "./Navbar/_components/logo";
 import Navbar from "./Navbar/navbar";
 import SecondSection from "./SecondSection/page";
+import SeventhSection from "./SeventhSection/page";
+import SixthSection from "./SixthSection/page";
 import ThirdSection from "./ThirdSection/page";
 
 
@@ -16,6 +19,9 @@ const LandingPage = () => {
       <ThirdSection/>
       <FourthSection/>
       <FifthSection/>
+      <SixthSection/>
+      <SeventhSection/>
+      <EighthSection/>
       
     </div>
   );
